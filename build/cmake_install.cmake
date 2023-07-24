15bf32be317806703f963b987efaa32a93345b0f
enc1 <yohoho4721@gmail.com>
2023-07-24 17:55:56 +0300

    on work

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/User/source/repos/otus-cpp-basics-lesson3
+# Install script for directory: C:/Users/AAleksandrov/dev/otus-cpp-basics-lesson3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/01.guess_the_number")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/User/source/repos/otus-cpp-basics-lesson3/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/AAleksandrov/dev/otus-cpp-basics-lesson3/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
